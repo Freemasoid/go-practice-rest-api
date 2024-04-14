6daf52dac6f43fab8306c2f5498732049e6fd5b9
Freemasoid <freemasoid@icloud.com>
2024-04-15 01:31:27 +0200

    setup middleware

---
+++ middleware/auth.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/Freemasoid/go-practice-rest-api/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
+		return
+	}
+
+	context.Next()
+}
