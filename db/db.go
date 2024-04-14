7303c6f6265d7f953f43c92ec9e126f6b40bfa6f
Freemasoid <freemasoid@icloud.com>
2024-04-15 01:50:49 +0200

    setup registrations table

--- db/db.go
+++ db/db.go
@@ -33,7 +33,7 @@ func createTables() {
 	_, err := DB.Exec(createUsersTable)
 
 	if err != nil {
-		panic("could not create a table")
+		panic("could not create users table")
 	}
 
 	createEventsTable := `
@@ -51,7 +51,22 @@ func createTables() {
 	_, err = DB.Exec(createEventsTable)
 
 	if err != nil {
-		panic("could not create a table")
+		panic("could not create events table")
 	}
 
+	createRegistrationsTable := `
+	CREATE TABLE IF NOT EXISTS registrations (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		event_id INTEGER,
+		user_id INTEGER
+		FOREIGN KEY(event_id) REFERENCES events(id)
+ 		FOREIGN KEY(user_id) REFERENCES users(id)
+	)
+	`
+
+	_, err = DB.Exec(createRegistrationsTable)
+
+	if err != nil {
+		panic("could not create registrations table")
+	}
 }
