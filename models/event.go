a3786e00b54cf2a98d3f625e571c53fae4cb15c2
Freemasoid <freemasoid@icloud.com>
2024-04-15 02:12:46 +0200

    cancel registration

--- models/event.go
+++ models/event.go
@@ -123,3 +123,17 @@ func (event Event) Register(userId int64) error {
 	_, err = stmt.Exec(event.ID)
 	return err
 }
+
+func (event Event) CancelRegistration(userId int64) error {
+	query := "DELETE FROM registrations WHERE event_id = ? AND user_id = ?"
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(event.ID)
+	return err
+}
